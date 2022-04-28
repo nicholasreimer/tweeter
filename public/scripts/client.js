6637f1301521645ff086ae4ead33440c3a86abd7
nicholasreimer <nchlsreimer@gmail.com>
2022-04-28 23:08:20 +0000

    W4 D4: Responsive Design - added a suite of responsive design edits, the web app now works on both desktop and tablet with proper styling.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,22 +1,22 @@
 //Client-side JS Logic:
 
 $(document).ready(function () {
-//--------------------------------------------------------------------------------------------------  
-//ESCAPE FUNCTION: used for eliminating Cross site scripting weaknesses
+  //--------------------------------------------------------------------------------------------------
+  //ESCAPE FUNCTION: used for eliminating Cross site scripting weaknesses
 
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-//-----------------------------------------------------------------
-//CREATE TWEET ELEMENT:
-function createTweetElement(tweetObj) {
-  //create the foundation for a new tweet in the html
-  let $newTweet = $(`<article class="tweetcontainer"></article>`);
+  //-----------------------------------------------------------------
+  //CREATE TWEET ELEMENT:
+  function createTweetElement(tweetObj) {
+    //create the foundation for a new tweet in the html
+    let $newTweet = $(`<article class="tweetcontainer"></article>`);
 
-  const htmlContent = `<header class="tweetheader">
+    const htmlContent = `<header class="tweetheader">
   <img src=${tweetObj.user.avatars}>
   <div>${tweetObj.user.name}</div>
   <div>${tweetObj.user.handle}</div>
@@ -34,79 +34,69 @@ function createTweetElement(tweetObj) {
   </div>
   </footer>`;
 
- let tweetElement = $newTweet.append(htmlContent);
+    let tweetElement = $newTweet.append(htmlContent);
 
-  return tweetElement;
-}
+    return tweetElement;
+  }
 
-//---------------------------------------------------------------
-//RENDER TWEETS:
-function renderTweets(tweetObjArray) {
-for(element of tweetObjArray){
-  $(".tweets-container").prepend(createTweetElement(element));
-}
-};
+  //---------------------------------------------------------------
+  //RENDER TWEETS:
+  function renderTweets(tweetObjArray) {
+    for (element of tweetObjArray) {
+      $(".tweets-container").prepend(createTweetElement(element));
+    }
+  }
 
-//-----------------------------------------------------------------
-//LOAD TWEETS: responsible for fetching tweets from the /tweets page
+  //-----------------------------------------------------------------
+  //LOAD TWEETS: responsible for fetching tweets from the /tweets page
 
-function loadTweets(){
+  function loadTweets() {
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:8080/tweets/",
+    }).then((response) => {
+      $(".tweets-container").empty();
+      renderTweets(response);
+    });
+    //use jQuery to make a request to /tweets and receive the array of tweets as JSON.
+  }
 
- $.ajax({
-  type: "GET",
-  url: "http://localhost:8080/tweets/",
-})
-.then((response) => {
-$(".tweets-container").empty();
-  renderTweets(response);
-})
-//use jQuery to make a request to /tweets and receive the array of tweets as JSON.
-}
+  loadTweets();
 
-loadTweets();
+  //----------------------------------------------------------------------
+  //FORM EVENT HANDLER and AJAX POST REQUEST
 
-//----------------------------------------------------------------------
-//FORM EVENT HANDLER and AJAX POST REQUEST
+  $("#create-tweet-form").submit(function (event) {
+    event.preventDefault();
 
-$( "#create-tweet" ).submit(function( event ) {
-  event.preventDefault();
-  
-  //Serialize the form data and send it to the server as a query string.
-  const $inputs = $("#tweet-text").serialize();
+    //Serialize the form data and send it to the server as a query string.
+    const $inputs = $("#tweet-text").serialize();
 
-if($(".error").is(":visible")) {
-  $(".error").slideUp();
-} 
+    console.log($inputs);
 
-//remove the "text=" that is inside of $inputs and check its length to be sure its under 140
-if($inputs.replace("text=", "").length > 140) {
- $(".error").slideDown();
-  return;
-}
+    if ($(".error").is(":visible")) {
+      $(".error").slideUp();
+    }
 
-if(!$inputs.replace("text=", "")){
-  $(".error").slideDown();
-  return;
-}
+    //remove the "text=" that is inside of $inputs and check its length to be sure its under 140
+    if ($inputs.replace("text=", "").length > 140) {
+      $(".error").slideDown();
+      return;
+    }
 
- $.ajax({
-  type: "POST",
-  url: "http://localhost:8080/tweets/",
-  data: $inputs,
-})
-.then(() => {
-console.log("have u made it this far?")
-loadTweets();
-})
+    if (!$inputs.replace("text=", "")) {
+      $(".error").slideDown();
+      return;
+    }
 
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/tweets/",
+      data: $inputs,
+    }).then(() => {
+      loadTweets();
+    });
+  });
+
+  //DOCUMENT READY
 });
-
-
-//MUST BE BOTTOM!
-});
-
-
-
-
-
-
